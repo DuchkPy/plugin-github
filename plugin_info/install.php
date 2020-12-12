cdcd3e0b7e74aa0a1be76558205a92a8d2210de6
hugoKs3 <hugo@questroy.com>
2020-12-12 20:41:49 +0100

    update script

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -32,6 +32,39 @@ function github_install() {
 }
 
 function github_update() {
+    foreach (eqLogic::byType('github') as $eqLogic) {
+		if ($eqLogic->getConfiguration('type') == 'repo') {        
+            $cmd = $eqLogic->getCmd(null, 'daily_unique_clones');
+            if ( ! is_object($cmd)) {
+                $cmd = new githubCmd();
+                $cmd->setName('Clones par jour');
+                $cmd->setEqLogic_id($eqLogic->getId());
+                $cmd->setLogicalId('daily_unique_clones');
+                $cmd->setType('info');
+                $cmd->setSubType('numeric');
+                $cmd->setIsHistorized(1);
+                $cmd->setTemplate('dashboard','tile');
+                $cmd->setTemplate('mobile','tile');
+                $cmd->setOrder(5);
+                $cmd->save();
+            }
+            $cmd = $eqLogic->getCmd(null, 'daily_unique_views');
+            if ( ! is_object($cmd)) {
+                $cmd = new githubCmd();
+                $cmd->setName('Vues par jour');
+                $cmd->setEqLogic_id($eqLogic->getId());
+                $cmd->setLogicalId('daily_unique_views');
+                $cmd->setType('info');
+                $cmd->setSubType('numeric');
+                $cmd->setIsHistorized(1);
+                $cmd->setTemplate('dashboard','tile');
+                $cmd->setTemplate('mobile','tile');
+                $cmd->setOrder(6);
+                $cmd->save();
+            }
+		}
+		$eqLogic->save();
+	}
 }
 
 function github_remove() {
